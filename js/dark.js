9cb4005b6067cbe16974978c2972d6aec8be9f91
iixCarbonxZz <iixcarbonxzz@gmail.com>
2020-12-14 20:54:42 +0000

    Moved dark mode to a separate file.

---
+++ js/dark.js
@@ -0,0 +1,16 @@
+//Nightmode button logic.
+function nightMode(){
+  var stylesheet = document.getElementById("stylesheet");
+  var label = document.getElementById("darkLabel");
+  //var logo = document.getElementById("bannerImage");
+  if(stylesheet.getAttribute("href") == "css/stylesheet-default.css"){
+    stylesheet.setAttribute("href", "css/stylesheet-nocturnal.css");
+    label.innerHTML = "Light Mode";
+    //logo.setAttribute("src", "discord-Logo-White.png");
+  }
+  else{
+    stylesheet.setAttribute("href", "css/stylesheet-default.css");
+    label.innerHTML = "Dark Mode";
+    //logo.setAttribute("src", "discord-Logo-Color.png");
+  }
+}
