d2a4227aac863b1f81f5686f5d561d45cf5881a8
iixCarbonxZz <iixcarbonxzz@gmail.com>
2020-12-15 14:10:14 +0000

    Added defer to dark.js in index. Added comments to dark.js.

--- js/dark.js
+++ js/dark.js
@@ -1,23 +1,33 @@
-//Nightmode button logic.
-window.addEventListener("load", checkMode());
+//DARK MODE BUTTON LOGIC
 
+///Event Listener to check the currently selected theme.
+window.addEventListener("load", checkMode, false);
+
+//Function to check the currently selected theme.
 function checkMode(){
+  //get mdoe variable from sites localStorage.
   var mode = localStorage.getItem("mode");
+  //Get dark mode button.
   var label = document.getElementById("darkLabel");
+  //Check the state of mode var.
   if(mode != ""){
+    //If mode is light, use the light stylesheet and label the button dark.
     if(mode == "Light"){
       stylesheet.setAttribute("href", "css/stylesheet-default.css");
       label.innerHTML = "Dark Mode";
     }
+    //If mode is dark, use the dark stylesheet and label the button light.
     else{
       stylesheet.setAttribute("href", "css/stylesheet-nocturnal.css");
       label.innerHTML = "Light Mode";
     }
   }
+  //If no mode is defined default to the light stylesheet and label the button dark.
   else{
     label.innerHTML = "Dark Mode";
   }
 }
+
 
 function nightMode(){
   var stylesheet = document.getElementById("stylesheet");
