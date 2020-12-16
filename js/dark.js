fd5123404cf619e533a9aa29bb2c0aab30135a77
iixCarbonxZz <iixcarbonxzz@gmail.com>
2020-12-16 17:26:25 +0000

    fixed syntax of nodeList.length references in dark.js.

--- js/dark.js
+++ js/dark.js
@@ -35,11 +35,11 @@ function nightMode(){
   var buttonList = document.getElementsByClassName("dashButton");
   //var logo = document.getElementById("bannerImage");
   if(stylesheet.getAttribute("href") == "css/stylesheet-default.css"){
-    for (var i = 0; i < buttonList.length(); i++){
+    for (var i = 0; i < buttonList.length; i++){
         buttonList.item(i).style.transition = "background-color 0s, color 0s, background-image 0s";
     }
     stylesheet.setAttribute("href", "css/stylesheet-nocturnal.css");
-    for (var i = 0; i < buttonList.length(); i++){
+    for (var i = 0; i < buttonList.length; i++){
         buttonList.item(i).removeAttribute("style");
     }
     label.innerHTML = "Light Mode";
@@ -47,11 +47,11 @@ function nightMode(){
     //logo.setAttribute("src", "discord-Logo-White.png");
   }
   else{
-    for (var i = 0; i < buttonList.length(); i++){
+    for (var i = 0; i < buttonList.length; i++){
         buttonList.item(i).style.transition = "background-color 0s, color 0s, background-image 0s";
     }
     stylesheet.setAttribute("href", "css/stylesheet-default.css");
-    for (var i = 0; i < buttonList.length(); i++){
+    for (var i = 0; i < buttonList.length; i++){
         buttonList.item(i).removeAttribute("style");
     }
     label.innerHTML = "Dark Mode";
