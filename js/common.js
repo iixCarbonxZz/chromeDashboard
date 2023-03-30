2f1918c826b806eb109984f92f322321e71bb1fb
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 22:43:21 +0100

    fixed more syntax errors in common.js

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 //Add CSS Hover Effects
 
-document.addEventListener("DOMContentLoaded"), function(){
+document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelector(".dashButton");
   var buttonLogo = document.querySelector(".dashButton > fab");
 
   button.addEventListener("mouseenter", function(e){
       buttonLogo.classList.add("fa-beat-fade");
   });
-};
+});
