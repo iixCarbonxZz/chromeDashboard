9939e4ecd61fdcd1daa77479520ac0f5da8f1fe0
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 23:13:19 +0100

    added ability to remove on mouseleave

--- js/common.js
+++ js/common.js
@@ -2,11 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelectorAll(".dashButton");
-  var buttonLogo = document.querySelectorAll(".dashButton > .fab");
+  var buttonLogo = document.querySelectorAll(".dashButton > .fab .fas");
 
     for(let i=0; i< button.length; i++){
         button[i].addEventListener("mouseenter", function(e){
         buttonLogo[i].classList.add("fa-beat-fade");
+        button[i].addEventListener("mouseleave", function(e){
+        buttonLogo[i].classList.remove("fa-beat-fade");
     });
   }
 });
