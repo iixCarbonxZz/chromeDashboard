525705debdeae2da731c14ccc8a314f2c55a5a6e
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 23:55:28 +0100

    more rotation fixes for roblox logo

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelectorAll(".dashButton");
   var buttonLogo = document.querySelectorAll(".dashButton > .buttonLogo");
+  var roblox = document.querySelector(".robloxLogo");
 
     for(let i=0; i< button.length; i++){
         button[i].addEventListener("mouseenter", function(e){
@@ -12,4 +13,12 @@ document.addEventListener("DOMContentLoaded", function(){
         buttonLogo[i].classList.remove("fa-beat-fade");
         });
     };
+    roblox.addEventListener("mouseenter", function(e){
+      roblox.classList.add("fa-rotate-by");
+      roblox.style.add("--fa-rotate-angle: 45deg;");
+    });
+    roblox.addEventListener("mouseleave", function(e){
+      roblox.classList.add("fa-rotate-by");
+      roblox.style.add("--fa-rotate-angle: 45deg;");
+    });
 });
