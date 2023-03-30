a8e5b814957f2c4e5e894d4f012153ceaac4bc18
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 23:57:23 +0100

    more rotation fix

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelectorAll(".dashButton");
   var buttonLogo = document.querySelectorAll(".dashButton > .buttonLogo");
-  var roblox = document.querySelector(".robloxLogo");
+  var roblox = document.querySelectorAll(".robloxLogo");
 
     for(let i=0; i< button.length; i++){
         button[i].addEventListener("mouseenter", function(e){
@@ -12,13 +12,13 @@ document.addEventListener("DOMContentLoaded", function(){
         button[i].addEventListener("mouseleave", function(e){
         buttonLogo[i].classList.remove("fa-beat-fade");
         });
+        roblox.addEventListener("mouseenter", function(e){
+          roblox.classList.add("fa-rotate-by");
+          roblox.style.cssText("--fa-rotate-angle: 45deg;");
+        });
+        roblox.addEventListener("mouseleave", function(e){
+          roblox.classList.add("fa-rotate-by");
+          roblox.style.cssText("--fa-rotate-angle: 45deg;");
+        });
     };
-    roblox.addEventListener("mouseenter", function(e){
-      roblox.classList.add("fa-rotate-by");
-      roblox.style.add("--fa-rotate-angle: 45deg;");
-    });
-    roblox.addEventListener("mouseleave", function(e){
-      roblox.classList.add("fa-rotate-by");
-      roblox.style.add("--fa-rotate-angle: 45deg;");
-    });
 });
