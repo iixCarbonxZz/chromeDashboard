fb1a093e3eb46ec57d49647bfdd90ea3bb9688c9
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 23:27:25 +0100

    addition of another class to reference in common.js

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   var button = document.querySelectorAll(".dashButton");
-  var buttonLogo = document.querySelectorAll(".dashButton > .fab .fas");
+  var buttonLogo = document.querySelectorAll(".dashButton > .buttonLogo");
 
     for(let i=0; i< button.length; i++){
         button[i].addEventListener("mouseenter", function(e){
