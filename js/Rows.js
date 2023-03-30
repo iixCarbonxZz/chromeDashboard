9a7c422d4a920664a4f27adf800d345657a59f78
iixCarbonxZz <iixcarbonxzz@gmail.com>
2023-03-30 22:36:49 +0100

    separated animation code into common.js

--- js/Rows.js
+++ js/Rows.js
@@ -29,15 +29,3 @@ function removeRow(){
     document.getElementById("tableMain").deleteRow(-1);
   }
 }
-
-
-//Add CSS Hover Effects
-
-document.addEventListener("DOMContentLoaded"), function(){
-  var button = document.querySelector(".dashButton");
-  var buttonLogo = document.querySelector(".dashButton > fab");
-
-  button.addEventListener("mouseenter", function(e){
-      buttonLogo.classList.add("fa-beat-fade");
-  });
-});
